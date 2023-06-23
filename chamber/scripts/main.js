a65fc130d2ae6696aba962e7fa503175730f41e7
Fex-zi <fexzi87@gmail.com>
2023-06-23 16:51:17 -0600

    week 03

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -7,3 +7,31 @@ hamburgerButton.addEventListener('click', () => {
 });
 
 
+const sidebarContent = document.querySelector('.Visits-cont');
+const lastVisit = localStorage.getItem('lastVisit');
+
+  if (!lastVisit) {
+    sidebarContent.textContent = "Welcome! Let us know if you have any questions.";
+  } else {
+    const lastVisitDate = new Date(parseInt(lastVisit));
+    const currentDate = new Date();
+    const timeDiff = Math.floor((currentDate - lastVisitDate) / (1000 * 60 * 60 * 24));
+
+    if (timeDiff === 0) {
+      sidebarContent.textContent = "Back so soon! Awesome!";
+    } else {
+      const timeMessage = `You last visited ${timeDiff} ${timeDiff === 1 ? 'day' : 'days'} ago.`;
+      sidebarContent.textContent = timeMessage;
+    }
+  }
+
+  localStorage.setItem('lastVisit', Date.now().toString());
+
+
+
+
+
+
+
+
+
